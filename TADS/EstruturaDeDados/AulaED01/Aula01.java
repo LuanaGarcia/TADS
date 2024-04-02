d8c9752e4c5bbf7bf3f6288189cef139d6d07fb9
Luana Garcia <luanaoliveirafss@gmail.com>
2024-04-02 10:22:26 -0300

    ajuste1

--- TADS/EstruturaDeDados/AulaED01/Aula01.java
+++ TADS/EstruturaDeDados/AulaED01/Aula01.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 
 public class Aula01 {
+
   public static void main(String[] args) {
     System.out.println("Olarrrrrr javeiros");
 
@@ -11,23 +12,18 @@ public class Aula01 {
 //    System.out.println("A soma dos números é: "+ soma);
 
       Scanner scanner = new Scanner(System.in);
-
       System.out.println("Digite um número: ");
       int num = scanner.nextInt();
         if (num <= 0){
           System.out.println("Erro no cálculo, o número não pode ser menor que zero");
         } else{
           System.out.println("Digite mais um número: ");
-          int num2 = scanner.nextInt();
-  
-        
+          int num2 = scanner.nextInt();        
         if (num2 <= 0){
           System.out.println("Erro no cálculo, o número não pode ser menor que zero");
         } else{
           System.out.println("A soma dos números é: "+ (num+num2));
-        }
-          
-        }
-      
+        }          
+        }  
     }
   }
