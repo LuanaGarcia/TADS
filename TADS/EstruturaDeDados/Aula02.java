eaad3c89703f76983d744804e772647db08513bc
Luana Garcia <luanaoliveirafss@gmail.com>
2024-04-03 09:26:05 -0300

    FinalAula

--- TADS/EstruturaDeDados/Aula02.java
+++ TADS/EstruturaDeDados/Aula02.java
@@ -3,30 +3,46 @@ import java.util.Scanner;
 public class Aula02 {
 
   public static void main(String[] args) {
-   //Mostrando a string em array de char 
-//    String curso = "TADS - Terceiro período";
+    // Mostrando a string em array de char
+    // String curso = "TADS - Terceiro período";
+
+    // for (int i = 0; i < curso.length(); i++){
+    // System.out.println(curso.charAt(i));
+    // }
+
+    // Definindo um Array/vetor
+    int[] nota = new int[4];
+    // nota[0] = 10;
+    // nota[1] = 10;
+    // nota[2] = 10;
+    // nota[3] = 10;
+
+    // Scanner scanner = new Scanner(System.in);
+
+    // for (int i = 0; i < 4; i++){
+    // System.out.println("Digite a nota nº " + (i+1));
+    // nota[i] = scanner.nextInt();
+    // }
+
+    // for (int i = 0; i < 4; i++){
+    // System.out.println("O valor do item " + (i+1) + " é: " + nota[i]);
+    // }
+
+    // Trabalhando com matrizes - array de 2 ou mais dimensões
     
-//    for (int i = 0; i < curso.length(); i++){
-//        System.out.println(curso.charAt(i));
-//    }
-      
-      // Definindo um Array/vetor
-      int[] nota = new int[4];
-//      nota[0] = 10;
-//      nota[1] = 10;
-//      nota[2] = 10;
-//      nota[3] = 10;
+    int[][] matriz = new int[3][3];
 
-    Scanner scanner = new Scanner(System.in);
-
-      for (int i = 0; i < 4; i++){
-        System.out.println("Digite a nota nº " + (i+1));
-        nota[i] = scanner.nextInt();
+      int cont = 1;
+      for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++){
+          matriz[i][j] = cont;
+          cont++;
+        }
       }
-
-        for (int i = 0; i < 4; i++){
-        System.out.println("O valor do item " + (i+1) + " é: " + nota[i]);
+      for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++){
+          System.out.println(matriz [i][j]);
+        }
       }
-
   }
 }
