40cf612a1b0291bab4903111cfd97f44a2009727
Luana Garcia <luanaoliveirafss@gmail.com>
2024-04-04 14:28:52 -0300

    atualização

---
+++ TADS/EstruturaDeDados/Aula04.java
@@ -0,0 +1,16 @@
+public class Aula04 {
+
+    public static void main(String[] args) {
+      
+      int numero1 = 10;
+      System.out.println(numero1);
+
+      numero1 = 20;
+      System.out.println(numero1);
+
+      final int CONSTANTE = 100;
+      System.out.println(CONSTANTE);
+
+
+    }
+}
